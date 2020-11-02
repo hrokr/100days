54c42aca0dee69304e31f7f66705295c8311856c
hrokr <alex.rook@gmail.com>
2020-11-01 20:55:56 -0500

    added convert_eu_to_us_date2 refator as one liner

--- day04-05.py
+++ day04-05.py
@@ -44,6 +44,17 @@ def convert_eu_to_us_date(date):
     return dto.strftime("%m/%d/%Y")
 
 
+# This is just the above refactored as a one-liner
+
+def convert_eu_to_us_date2(date):
+    """Receives a date string in European format of dd/mm/yyyy, e.g. 11/03/2002
+       Convert it to an American date: mm/dd/yyyy (in this case 03/11/2002).
+       To enforce the use of datetime's strptime / strftime (over slicing)
+       the tests check if a ValueError is raised for invalid day/month/year
+       ranges (no need to code this, datetime does this out of the box)"""
+    return datetime.strptime(date, "%d/%m/%Y").strftime("%m/%d/%Y")
+
+
 # Additional stuff that interested me
 
 def ending_date():
@@ -52,8 +63,9 @@ def ending_date():
 
 
 if __name__ == "__main__":
-    print(get_hundred_days_end_date())
-    print(get_days_between_pb_start_first_joint_pycon(pybites_founded, pycon_date))
-    ending_date()
-    print(years_ago("23 Mar, 2020"))
-    print(convert_eu_to_us_date('11/03/2002'))
+    # print(get_hundred_days_end_date())
+    # print(get_days_between_pb_start_first_joint_pycon(pybites_founded, pycon_date))
+    # ending_date()
+    # print(years_ago("23 Mar, 2020"))
+    # print(convert_eu_to_us_date('11/03/2002'))
+    print(convert_eu_to_us_date2('11/03/2002'))
